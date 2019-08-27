0a0582081be5dce04c3cc51c75f29dac1375b6c5
Julio Cavallari <53496995+julinhoooO@users.noreply.github.com>
2019-08-27 10:30:34 -0300

    Add files via upload

--- src/pages/FormGrade/index.js
+++ src/pages/FormGrade/index.js
@@ -1,68 +1,236 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {DatePickerAndroid, Keyboard} from 'react-native';
+import {StackActions, NavigationActions} from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {TextField} from 'react-native-material-textfield';
+
+import getRealm from '../../services/realm';
+
+import {getStringDate, formatDateBR} from '../../functions';
+
+import {
+  Placeholder,
+  PlaceholderMedia,
+  PlaceholderLine,
+  Shine,
+} from 'rn-placeholder';
 
 import {
   Container,
   Title,
   Form,
   Input,
-  ButtonsContainer,
+  EmptyContainer,
   SubmitButton,
   SubmitButtonText,
+  ViewDateText,
+  TextViewDate,
 } from './styles';
 
 export default function FormGrade({navigation}) {
   const {params} = navigation.state;
   const [name, setName] = useState('');
   const [grade, setGrade] = useState('');
+  const [gradeState, setGradeState] = useState([]);
   const [maximumGrade, setMaximumGrade] = useState('');
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [dateText, setDateText] = useState('');
+  const [errorName, setErrorName] = useState('');
+  const [errorGrade, setErrorGrade] = useState('');
+  const [errorMaximumGrade, setErrorMaximumGrade] = useState('');
+  const [errorDate, setErrorDate] = useState('');
+  const [update, setUpdate] = useState(params.update);
+  const [titleName, setTitleName] = useState('');
+  const [placeholderDate, setPlaceholderDate] = useState(true);
+
+  useEffect(() => {
+    if (update) {
+      async function getGradeData() {
+        const realm = await getRealm();
+        const gradeData = realm
+          .objects('Grade')
+          .sorted('name')
+          .filtered(`id == ${params.grade_id}`);
+        setGradeState(gradeData[0]);
+        setName(gradeData[0].name);
+        setGrade(gradeData[0].grade.toString());
+        setMaximumGrade(gradeData[0].maximum_grade.toString());
+        setDate(gradeData[0].date);
+        setDateText(formatDateBR(getStringDate(new Date(gradeData[0].date))));
+        setPlaceholderDate(false);
+        setTitleName(gradeData[0].name);
+        setUpdate(false);
+      }
+      getGradeData();
+    }
+  }, []);
+
+  function handleSaveButton() {
+    if (name === false || name === null || name === '') {
+      setErrorName('Não pode ser vazio');
+    }
+
+    if (grade === false || grade === null || grade === '') {
+      setErrorGrade('Não pode ser vazio');
+    }
+
+    if (
+      maximumGrade === false ||
+      maximumGrade === null ||
+      maximumGrade === ''
+    ) {
+      setErrorMaximumGrade('Não pode ser vazio');
+    }
+
+    if (date === false || date === null || date === '') {
+      setErrorDate('Data inválida');
+    }
+
+    if (!errorName && !errorGrade && !errorMaximumGrade && !errorDate) {
+      saveGrade(params.update);
+    }
+  }
+
+  async function saveGrade(update = false) {
+    const realm = await getRealm();
+    //Search for all data and sort by id DESC
+    const gradeRealm = realm.objects('Grade').sorted('id', true);
+    //Get the maximum id to simulate a Auto_Increment
+    const lastIdInserted = gradeRealm.length ? gradeRealm.slice(0, 1)[0].id : 0;
+    const data = {
+      id: lastIdInserted ? parseInt(lastIdInserted + 1) : 1,
+      id_disciplina: params.data.id,
+      name: name,
+      grade: parseInt(grade),
+      maximum_grade: parseInt(maximumGrade),
+      date,
+    };
+    if (update) {
+      data.id = params.grade_id;
+    }
+
+    realm.write(() => {
+      realm.create('Grade', data, 'modified');
+    });
+
+    const navigateToAulas = StackActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({
+          routeName: 'Details',
+          params: {data: params.data},
+        }),
+      ],
+    });
+    navigation.dispatch(navigateToAulas);
+  }
+  async function toggleDatePicker() {
+    try {
+      const {action, year, month, day} = await DatePickerAndroid.open({
+        date: new Date(date),
+      });
+      if (action !== DatePickerAndroid.dismissedAction) {
+        setDate(new Date(year, month, day));
+        setDateText(formatDateBR(getStringDate(new Date(year, month, day))));
+        setPlaceholderDate(false);
+      }
+    } catch ({code, message}) {
+      console.warn('Cannot open date picker', message);
+    }
+  }
   return (
     <Container>
-      <Title>Adicionar Novo Trabalho/Prova</Title>
+      {params.update === true && gradeState.length === 0 ? (
+        <Placeholder Animation={Shine}>
+          <PlaceholderLine
+            height={48}
+            width={50}
+            style={{borderRadius: 4, marginLeft: 20}}
+          />
+        </Placeholder>
+      ) : (
+        <Title>
+          {params.update
+            ? `Editar ${titleName}`
+            : 'Adicionar Novo Trabalho/Prova'}
+        </Title>
+      )}
       <Form>
-        <Input
-          value={name}
-          onChangeText={setName}
-          autoCorrect={false}
-          autoCompleteType="off"
-          placeholder="Digite o nome do Trabalho/Prova"
-        />
-        <Input
-          value={grade}
-          onChangeText={setGrade}
-          autoCapitalize="none"
-          autoCompleteType="off"
-          autoCorrect={false}
-          keyboardType="number-pad"
-          placeholder="Digite sua nota"
-        />
-        <Input
-          value={maximumGrade}
-          onChangeText={setMaximumGrade}
-          autoCapitalize="none"
-          autoCompleteType="off"
-          autoCorrect={false}
-          keyboardType="number-pad"
-          placeholder="Digite o peso do Trabalho/Prova"
-        />
-        <Input
-          value={date}
-          onChangeText={text => {
-            setDate(text.replace(/(\d\d)(\d\d)(\d\d\d\d)/, '$1/$2/$3'));
-          }}
-          autoCapitalize="none"
-          autoCompleteType="off"
-          autoCorrect={false}
-          keyboardType="number-pad"
-          placeholder="Digite a Data do Trabalho/Prova"
-          maxLength={10}
-        />
+        {params.update === true && gradeState.length === 0 ? (
+          <Placeholder Animation={Shine}>
+            <PlaceholderLine
+              height={52}
+              noMargin={true}
+              style={{borderRadius: 4, marginBottom: 10}}
+            />
+            <PlaceholderLine
+              height={52}
+              noMargin={true}
+              style={{borderRadius: 4, marginBottom: 10}}
+            />
+            <PlaceholderLine
+              height={52}
+              noMargin={true}
+              style={{borderRadius: 4, marginBottom: 10}}
+            />
+            <PlaceholderLine
+              height={52}
+              noMargin={true}
+              style={{borderRadius: 4, marginBottom: 10}}
+            />
+          </Placeholder>
+        ) : (
+          <>
+            <TextField
+              value={name}
+              label={'Nome'}
+              error={errorName}
+              tintColor={'#7159c1'}
+              baseColor={'#7159c1'}
+              onChangeText={setName}
+              title={'Digite o nome do trabalho/prova'}
+            />
+            <TextField
+              value={grade}
+              label={'Nota'}
+              error={errorGrade}
+              tintColor={'#7159c1'}
+              baseColor={'#7159c1'}
+              onChangeText={setGrade}
+              title={'Digite a nota do trabalho/prova'}
+              keyboardType={'numeric'}
+            />
+            <TextField
+              value={maximumGrade}
+              label={'Nota máxima'}
+              error={errorMaximumGrade}
+              tintColor={'#7159c1'}
+              baseColor={'#7159c1'}
+              onChangeText={setMaximumGrade}
+              title={'Digite a nota máxima para esse trabalho/prova'}
+              keyboardType={'numeric'}
+            />
+            <TextField
+              value={dateText}
+              label={'Data'}
+              error={errorDate}
+              tintColor={'#7159c1'}
+              baseColor={'#7159c1'}
+              title={'Selecione a data do evento'}
+              onFocus={() => {
+                Keyboard.dismiss();
+                toggleDatePicker();
+              }}
+            />
+            <SubmitButton onPress={handleSaveButton}>
+              <Icon name={'content-save'} size={24} color={'#fff'} />
+              <SubmitButtonText>Salvar</SubmitButtonText>
+              <EmptyContainer />
+            </SubmitButton>
+          </>
+        )}
       </Form>
-      <ButtonsContainer>
-        <SubmitButton onPress={() => {}}>
-          <SubmitButtonText>Salvar</SubmitButtonText>
-        </SubmitButton>
-      </ButtonsContainer>
+      <Form />
     </Container>
   );
 }
--- src/pages/FormGrade/styles.js
+++ src/pages/FormGrade/styles.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components/native';
 import {getStatusBarHeight} from 'react-native-status-bar-height';
 
+import * as themes from '../../themes';
+
 export const Container = styled.View`
   flex: 1;
-  background-color: #7159c1;
+  background-color: ${themes.light.card.backgroundColor};
   padding-top: ${30 + getStatusBarHeight(true)}px;
   justify-content: space-between;
 `;
@@ -13,38 +15,23 @@ export const Title = styled.Text`
   align-items: center;
   font-size: 32px;
   font-weight: bold;
-  color: #fff;
-  padding: 0 20px;
+  color: ${themes.light.title.color};
+  padding: 10px 20px 0;
 `;
 
 export const Form = styled.View`
   padding: 30px;
 `;
 
-export const Input = styled.TextInput.attrs({
-  placeHolderTextColor: '#999',
-})`
-  padding: 12px 15px;
-  border-radius: 4px;
-  background-color: #f1f1f1;
-  color: #333;
-  font-size: 16px;
-  margin-bottom: 10px;
-`;
-export const ButtonsContainer = styled.View`
-  flex-direction: row;
-  padding: 8px;
-`;
-
 export const SubmitButton = styled.TouchableOpacity`
-  flex: 1;
-  height: 48px;
-  background-color: #00bbff;
-  justify-content: center;
+  flex-direction: row;
+  height: 52px;
+  background-color: ${themes.light.button.backgroundColor};
+  color: ${themes.light.button.color};
+  justify-content: space-between;
   align-items: center;
   border-radius: 4px;
   padding: 8px;
-  margin-left: 4px;
   shadowColor: #000;
   shadowOffset: { width: 0, height: 2 };
   shadowOpacity: 0.8;
@@ -53,7 +40,11 @@ export const SubmitButton = styled.TouchableOpacity`
 `;
 
 export const SubmitButtonText = styled.Text`
+  margin-left: -24px;
+  align-self: center;
   font-size: 20px;
   font-weight: bold;
-  color: #fff;
+  color: ${themes.light.button.color};
 `;
+
+export const EmptyContainer = styled.View``;
