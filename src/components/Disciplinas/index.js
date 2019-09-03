74a799179202adf2e3424702781b8d42abf5279f
Julio Cavallari <53496995+julinhoooO@users.noreply.github.com>
2019-09-03 13:22:51 -0300

    Add files via upload

--- src/components/Disciplinas/index.js
+++ src/components/Disciplinas/index.js
@@ -1,19 +1,153 @@
-import React from 'react';
-import {withNavigation} from 'react-navigation';
+import React, {useState} from 'react';
+import {
+  withNavigation,
+  StackActions,
+  NavigationActions,
+} from 'react-navigation';
+import {Vibration} from 'react-native';
+import {
+  TouchableRipple,
+  Modal,
+  Portal,
+  List,
+  Dialog,
+  Paragraph,
+  Button,
+  Snackbar,
+  Divider,
+} from 'react-native-paper';
 
-import {Container, Name, ToggleCollapsibleContainer} from './styles';
+import getRealm from '../../services/realm';
 
-function Disciplinas({data, navigation}) {
+import {Container, Name, RowContainer} from './styles';
+
+function DisciplinasComponent({data, navigation}) {
+  const [visibleDisciplinaDialog, setVisibleDisciplinaDialog] = useState(false);
+  const [visibleConfirmation, setVisibleConfirmation] = useState(false);
+
+  async function deleteDisciplina() {
+    setVisibleDisciplinaDialog(false);
+    setVisibleConfirmation(false);
+    const realm = await getRealm();
+    const disciplinaData = realm
+      .objects('Disciplina')
+      .filtered(`id == ${data.id}`);
+    realm.write(() => {
+      realm.delete(disciplinaData[0]);
+    });
+    const navigateToAulas = StackActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({
+          routeName: 'Disciplinas',
+          params: {
+            delete: true,
+          },
+        }),
+      ],
+    });
+    navigation.dispatch(navigateToAulas);
+  }
+
   return (
-    <Container>
-      <ToggleCollapsibleContainer
+    <>
+      <Portal>
+        <Modal
+          visible={visibleDisciplinaDialog}
+          onDismiss={() => {
+            setVisibleDisciplinaDialog(false);
+          }}>
+          <List.Section
+            title={data.name}
+            titleStyle={{
+              fontWeight: 'bold',
+              color: '#000',
+              fontSize: 18,
+            }}
+            style={{
+              marginHorizontal: 18,
+              borderRadius: 2,
+              backgroundColor: '#fff',
+            }}>
+            <Divider />
+            <List.Item
+              title="Excluir"
+              left={props => (
+                <List.Icon {...props} color={'#000'} icon="delete" />
+              )}
+              onPress={() => {
+                setVisibleDisciplinaDialog(false);
+                setVisibleConfirmation(true);
+              }}
+              titleStyle={{
+                color: '#000',
+              }}
+              style={{height: 56}}
+            />
+            <List.Item
+              title="Editar"
+              left={props => (
+                <List.Icon {...props} color={'#000'} icon="edit" />
+              )}
+              onPress={() => {
+                setVisibleDisciplinaDialog(false);
+                setVisibleConfirmation(false);
+                setTimeout(() => {
+                  navigation.navigate('FormDisciplina', {
+                    update: true,
+                    id: data.id,
+                  });
+                }, 180);
+              }}
+              titleStyle={{
+                color: '#000',
+              }}
+            />
+          </List.Section>
+        </Modal>
+        <Dialog
+          style={{borderRadius: 2}}
+          visible={visibleConfirmation}
+          onDismiss={() => {
+            setVisibleConfirmation(false);
+          }}
+          style={{elevation: 4}}>
+          <Dialog.Title>Excluir</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph style={{fontSize: 16}}>
+              Tem certeza que deseja excluir a disciplina {data.name}
+            </Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={() => {
+                setVisibleConfirmation(false);
+              }}>
+              Cancelar
+            </Button>
+            <Button onPress={deleteDisciplina}>Excluir</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+      <TouchableRipple
+        underlayColor={'rgba(113, 89, 193, .16)'}
+        borderless={true}
+        rippleColor={'rgba(113, 89, 193, .16)'}
         onPress={() => {
           navigation.navigate('Aulas', {data});
+        }}
+        onLongPress={() => {
+          Vibration.vibrate([1, 50, 30, 50]);
+          setVisibleDisciplinaDialog(true);
         }}>
-        <Name>{data.name}</Name>
-      </ToggleCollapsibleContainer>
-    </Container>
+        <Container key={data.id}>
+          <RowContainer>
+            <Name>{data.name}</Name>
+          </RowContainer>
+        </Container>
+      </TouchableRipple>
+    </>
   );
 }
 
-export default withNavigation(Disciplinas);
+export default withNavigation(DisciplinasComponent);
--- src/components/Disciplinas/styles.js
+++ src/components/Disciplinas/styles.js
@@ -1,51 +1,43 @@
 import styled from 'styled-components/native';
 
+import * as themes from '../../themes';
+
 export const Container = styled.View`
-  flex: 1;
-  border-radius: 4px;
-  background-color: #fff;
-  margin-bottom: 15px;
-`;
-export const ToggleCollapsibleContainer = styled.TouchableOpacity`
-  flex: 1;
+  background-color: ${themes.light.card.backgroundColor};
+  padding: 4px;
+  height: 100px;
+  min-height: 100px;
+  max-height: 100px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${themes.light.card.borderColor};
 `;
 export const RowContainer = styled.View`
   flex: 1;
   flex-direction: row;
+  align-items: center;
 `;
 export const Name = styled.Text`
   flex: 1;
   font-size: 24px;
   font-weight: bold;
-  padding: 20px;
+  padding: 16px;
+  color: ${themes.light.card.color};
 `;
-export const Data = styled.View`
+export const ButtonsContainer = styled.View`
   justify-content: center;
   align-items: center;
-  margin-right: 10px;
-`;
-export const CollapsibleContainer = styled.View`
-  flex: 1;
-  padding: 20px;
-`;
-export const Hours = styled.View`
-  flex: 1;
-`;
-export const MissedHours = styled.Text`
-  font-size: 14px;
-  font-weight: bold;
-`;
-export const RamainingHours = styled.Text`
-  font-size: 14px;
-  font-weight: bold;
-`;
-export const GradesContainer = styled.View`
-  flex: 1;
-`;
-export const Grades = styled.View`
-  flex: 1;
-`;
-export const GradesData = styled.Text`
-  flex: 1;
   flex-direction: row;
+  padding-right: 12px;
+`;
+export const AddMissButton = styled.TouchableOpacity`
+  padding: 8px;
+  margin: 4px;
+  border-radius: 4px;
+  background-color: ${themes.light.button.backgroundColor};
+  color: ${themes.light.button.color};
+  shadowColor: #000;
+  shadowOffset: { width: 0, height: 2 };
+  shadowOpacity: 0.8;
+  shadowRadius: 2;
+  elevation: 2;
 `;
